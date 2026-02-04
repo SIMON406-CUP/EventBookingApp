9666a3a3e80b0774b803bc9f27242be516cf36ea
Moriswi Simon <simonmothoka40@gmail.com>
2026-02-04 09:05:32 +0200

    Added AppDbContext for database access

---
+++ EventBookingApp/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using EventBookingApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Event> Events { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+}
