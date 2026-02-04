70a04d4757e892e641f29ac55cbd67306cb3f652
Moriswi Simon <simonmothoka40@gmail.com>
2026-02-04 08:53:48 +0200

    Add project files.

---
+++ EventBookingApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EventBookingApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
