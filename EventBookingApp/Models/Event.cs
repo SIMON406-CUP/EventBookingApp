7d8964a15cc65b47fe1fcb70cace23a665d5a292
Moriswi Simon <simonmothoka40@gmail.com>
2026-02-04 08:56:52 +0200

    Added Event model for event booking system

---
+++ EventBookingApp/Models/Event.cs
@@ -0,0 +1,8 @@
+﻿public class Event
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Location { get; set; }
+    public DateTime Date { get; set; }
+    public int AvailableSeats { get; set; }
+}
